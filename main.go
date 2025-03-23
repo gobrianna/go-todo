1f2d5a5b670c3c4d577553e6f4689849884cbf04
Brianna <whoamibrianna@tuta.io>
2025-03-23 14:00:44 -0400

    Refactor code for readability

--- main.go
+++ main.go
@@ -4,5 +4,13 @@ import "fmt"
 
 // Prints a message to the console
 func main() {
-	fmt.Println("Look at all these chickens!")
+	taskOne := "Look at all these chickens!"
+	taskTwo := "Learn basics of Go!"
+	taskThree := "Build a mini-project using Go basics!"
+
+	fmt.Println("#### Todo List ####")
+	fmt.Println()
+	fmt.Println(taskOne)
+	fmt.Println(taskTwo)
+	fmt.Println(taskThree)
 }
