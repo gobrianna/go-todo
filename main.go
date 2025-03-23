d95f4bb79e284478afb70fbc34f1eb2a318c0196
Brianna <whoamibrianna@tuta.io>
2025-03-23 14:48:17 -0400

    Add slice and refactor

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
-// Prints a message to the console
+// Main initializes a to-do list and prints it to the console
 func main() {
 	taskOne := "Look at all these chickens!"
 	taskTwo := "Learn basics of Go!"
 	taskThree := "Build a mini-project using Go basics!"
+	taskFour := "Water the plants."
+	taskFive := "Play OoT."
+
+	// taskList is a slice of strings that holds the to-do items; can only hold elements of the same type
+	taskList := []string {taskOne, taskTwo, taskThree, taskFour, taskFive}
 
 	fmt.Println("#### Todo List ####")
-	fmt.Println()
-	fmt.Println(taskOne)
-	fmt.Println(taskTwo)
-	fmt.Println(taskThree)
+	// Prints taskList to the console
+	fmt.Println("Here's whacha gotta do:\n", taskList)
 }
