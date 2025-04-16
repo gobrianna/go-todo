6fb136970683b474993c931bfa3d88984816204c
Brianna <whoamibrianna@tuta.io>
2025-04-16 18:42:30 -0400

    refactor: encapsulate print logic in printTasks function

--- main.go
+++ main.go
@@ -2,21 +2,26 @@ package main
 
 import "fmt"
 
+// Displaying usage of global variables
+var (
+taskOne = "Look at all these chickens!"
+taskTwo = "Learn basics of Go!"
+taskThree = "Build a mini-project using Go basics!"
+taskFour = "Water the plants."
+taskFive = "Play OoT."
+)
+// taskList is a slice of strings that holds the to-do items; can only hold elements of the same type
+var taskList = []string {taskOne, taskTwo, taskThree, taskFour, taskFive}
+
 // Main initializes a to-do list and prints it to the console
 func main() {
-	taskOne := "Look at all these chickens!"
-	taskTwo := "Learn basics of Go!"
-	taskThree := "Build a mini-project using Go basics!"
-	taskFour := "Water the plants."
-	taskFive := "Play OoT."
-
-	// taskList is a slice of strings that holds the to-do items; can only hold elements of the same type
-	taskList := []string {taskOne, taskTwo, taskThree, taskFour, taskFive}
-
 	fmt.Println("#### Todo List ####")
+	printTasks()
+}
 
-	// for loop that iterates through taskList, returning the index from 1 and value of task
-	for index, task := range taskList {
-		fmt.Printf("%d. %s\n", index+1, task)
-	}
+// Iterates through taskList, printing each task with its position (starting from 1)
+func printTasks() {
+		for index, task := range taskList {
+			fmt.Printf("%d. %s\n", index+1, task)
+		}
 }
